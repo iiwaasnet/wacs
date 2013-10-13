b54beaf2f21d062e08a51a2122070718f5f4bc8c
iiwaasnet <givasena@gmail.com>
2013-10-13 14:36:36 +0200

    Unit testing

--- src/wacs/Tests/FLease/LeaseProviderTests.cs
+++ src/wacs/Tests/FLease/LeaseProviderTests.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
 using Moq;
 using NUnit.Framework;
 using wacs.FLease;
@@ -12,7 +14,7 @@ namespace wacs.Tests.FLease
 		[Test]
 		public void TestStartingLeaseProvider_StartsRoundBasedRegister()
 		{
-		var builder = DIHelper.CreateBuilder();
+			var builder = DIHelper.CreateBuilder();
 
 			var owner = new Process(12);
 			var register = new Mock<IRoundBasedRegister>();
@@ -26,6 +28,38 @@ namespace wacs.Tests.FLease
 			leaseProvider.Start();
 
 			register.Verify(m => m.Start(), Times.Once());
+		}
+
+		[Test]
+		public void TestStopingLeaseProvider_StopsRoundBasedRegister()
+		{
+			var builder = DIHelper.CreateBuilder();
+
+			var owner = new Process(12);
+			var register = new Mock<IRoundBasedRegister>();
+			var registerFactory = new Mock<IRoundBasedRegisterFactory>();
+			registerFactory.Setup(m => m.Build(It.Is<IProcess>(v => v.Id == owner.Id))).Returns(register.Object);
+
+			builder.Register(c => registerFactory.Object).As<IRoundBasedRegisterFactory>().SingleInstance();
+
+			var leaseProvider = builder.Build().Resolve<ILeaseProviderFactory>().Build(owner);
+
+			leaseProvider.Stop();
+
+			register.Verify(m => m.Stop(), Times.Once());
+		}
+
+		[Test]
+		public void TestGetLease_ReturnsTask()
+		{
+			var lease = new Lease(new Process(12), DateTime.UtcNow + TimeSpan.FromSeconds(3));
+			var leaseProvider = new Mock<ILeaseProvider>();
+			leaseProvider.Setup(m => m.GetLease()).Returns(Task.FromResult<ILease>(lease));
+
+			var acquiredLease = leaseProvider.Object.GetLease().Result;
+
+			Assert.AreEqual(lease.Owner.Id, acquiredLease.Owner.Id);
+			Assert.AreEqual(lease.ExpiresAt, acquiredLease.ExpiresAt);
 		}
 	}
 }
