d890c3a21e0345ae200c1efa77136010399bad06
iiwaasnet <givasena@gmail.com>
2013-12-01 18:58:41 +0100

    Refactoring

--- src/wacs/Program.cs
+++ src/wacs/Program.cs
@@ -8,58 +8,58 @@ using wacs.Diagnostics;
 
 namespace wacs
 {
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			var builder = new ContainerBuilder();
-			builder.RegisterModule(new ConfigurationSettingsReader());
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new ConfigurationSettingsReader());
 
-			var container = builder.Build();
+            var container = builder.Build();
 
-			HostFactory.New(x => ConfigureService(x, container)).Run();
-		}
+            HostFactory.New(x => ConfigureService(x, container)).Run();
+        }
 
-		private static void ConfigureService(HostConfigurator x, IContainer container)
-		{
-			x.Service<ServiceControl>(s =>
-				                          {
-					                          s.ConstructUsing(name => container.Resolve<ServiceControl>());
-					                          s.WhenStarted((sc, hc) => sc.Start(hc));
-					                          s.WhenStopped((sc, hc) => sc.Stop(hc));
-				                          });
-			x.RunAsLocalSystem();
+        private static void ConfigureService(HostConfigurator x, IContainer container)
+        {
+            x.Service<ServiceControl>(s =>
+                                      {
+                                          s.ConstructUsing(name => container.Resolve<ServiceControl>());
+                                          s.WhenStarted((sc, hc) => sc.Start(hc));
+                                          s.WhenStopped((sc, hc) => sc.Stop(hc));
+                                      });
+            x.RunAsLocalSystem();
 
-			x.SetDescription("Wait-free Coordination Service");
-			x.SetDisplayName("WACS");
-			x.SetServiceName("WACS");
+            x.SetDescription("Wait-free Coordination Service");
+            x.SetDisplayName("WACS");
+            x.SetServiceName("WACS");
 
-			x.AfterInstall(InstallPerfCounters);
-			x.BeforeUninstall(UninstallPerfCounters);
-		}
+            x.AfterInstall(InstallPerfCounters);
+            x.BeforeUninstall(UninstallPerfCounters);
+        }
 
-		private static void UninstallPerfCounters()
-		{
-			try
-			{
-				new PerformanceCountersInstaller<WacsPerformanceCounters>().Uninstall();
-			}
-			catch (Exception err)
-			{
-				Console.WriteLine(err);
-			}
-		}
+        private static void UninstallPerfCounters()
+        {
+            try
+            {
+                new PerformanceCountersInstaller<WacsPerformanceCounters>().Uninstall();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+        }
 
-		private static void InstallPerfCounters(InstallHostSettings settings)
-		{
-			try
-			{
-				new PerformanceCountersInstaller<WacsPerformanceCounters>().Install();
-			}
-			catch (Exception err)
-			{
-				Console.WriteLine(err);
-			}
-		}
-	}
+        private static void InstallPerfCounters(InstallHostSettings settings)
+        {
+            try
+            {
+                new PerformanceCountersInstaller<WacsPerformanceCounters>().Install();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+        }
+    }
 }
