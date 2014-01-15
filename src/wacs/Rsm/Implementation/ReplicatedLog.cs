4edd7d907292a9cf1bc44c6a607a58154eba5c21
iiwaasnet <givasena@gmail.com>
2014-01-15 10:42:09 +0100

    ReplicatedLog

---
+++ src/wacs/Rsm/Implementation/ReplicatedLog.cs
@@ -0,0 +1,22 @@
+﻿using wacs.Rsm.Interface;
+
+namespace wacs.Rsm.Implementation
+{
+    public class ReplicatedLog : IReplicatedLog
+    {
+        public ILogEntry GetLogEntry(ILogIndex iid)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetLogEntry(ILogIndex iid, ILogEntry value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ILogIndex GetFirstUnchosenLogEntry()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
