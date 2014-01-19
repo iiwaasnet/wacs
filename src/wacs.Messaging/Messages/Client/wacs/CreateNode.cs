a95ec35db334310a57292f8a98b583e69ae7017f
iiwaasnet <givasena@gmail.com>
2014-01-19 16:40:19 +0100

    ReplicatedState

---
+++ src/wacs.Messaging/Messages/Client/wacs/CreateNode.cs
@@ -0,0 +1,27 @@
+﻿using wacs.Configuration;
+
+namespace wacs.Messaging.Messages.Client.wacs
+{
+    public class CreateNode : TypedMessage<CreateNode.Payload>
+    {
+        public CreateNode(IMessage message)
+            : base(message)
+        {
+        }
+
+        public CreateNode(IProcess sender, Payload payload)
+            : base(sender, payload, MessageType)
+        {
+        }
+
+        public static string MessageType
+        {
+            get { return "WACS_CREATE_NODE"; }
+        }
+
+        public class Payload
+        {
+            
+        }
+    }
+}
--- src/wacs/Rsm/Implementation/WacsStateMachine.cs
+++ src/wacs/Rsm/Implementation/WacsStateMachine.cs
@@ -1,12 +1,38 @@
-﻿using wacs.Rsm.Interface;
+﻿using System;
+using System.Collections.Generic;
+using wacs.Rsm.Interface;
 
 namespace wacs.Rsm.Implementation
 {
     public class WacsStateMachine : IReplicatedStateMachine
     {
+        private readonly IDictionary<string, Action<ISyncCommand>> messageHandlers;
+
+        public WacsStateMachine()
+        {
+            messageHandlers = CreateMessageHandlersMap();
+        }
+
         public void ProcessCommand(ISyncCommand command)
         {
-            throw new System.NotImplementedException();
+            Action<ISyncCommand> handler;
+
+            if (messageHandlers.TryGetValue(command.Request.Body.MessageType, out handler))
+            {
+                handler(command);
+            }
+        }
+
+        private void CreateNode(ISyncCommand command)
+        {
+        }
+
+        private IDictionary<string, Action<ISyncCommand>> CreateMessageHandlersMap()
+        {
+            return new Dictionary<string, Action<ISyncCommand>>
+                   {
+                       {Messaging.Messages.Client.wacs.CreateNode.MessageType, CreateNode}
+                   };
         }
     }
 }
