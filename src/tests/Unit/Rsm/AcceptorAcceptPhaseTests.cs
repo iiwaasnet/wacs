8aa1ad56fb37c9b752dc2a671239efe6529a9d62
iiwaasnet <givasena@gmail.com>
2014-04-16 21:14:43 +0200

    Acceptor testing

---
+++ src/tests/Unit/Rsm/AcceptorAcceptPhaseTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Threading;
+using Moq;
+using NUnit.Framework;
+using wacs.Communication.Hubs.Intercom;
+using wacs.Configuration;
+using wacs.Diagnostics;
+using wacs.FLease;
+using wacs.Messaging.Messages;
+using wacs.Messaging.Messages.Intercom.Rsm;
+using wacs.Resolver;
+using wacs.Rsm.Implementation;
+using wacs.Rsm.Interface;
+using Ballot = wacs.Messaging.Messages.Intercom.Rsm.Ballot;
+using LogIndex = wacs.Rsm.Implementation.LogIndex;
+using Process = wacs.Configuration.Process;
+
+namespace tests.Unit.Rsm
+{
+    [TestFixture]
+    public class AcceptorAcceptPhaseTests
+    {
+        [Test]
+        public void OnAcceptRequestCameNotFromLeader_AcceptorRespondsWith_RsmNackAcceptNotLeader()
+        {
+            var leaderId = 1;
+            var proposerId = 2;
+            var proposedLogIndex = 120UL;
+            var proposalNumber = 100UL;
+
+            var leaseProvider = new Mock<ILeaseProvider>();
+            var lease = new Mock<ILease>();
+            var leader = new Process(leaderId);
+            lease.Setup(m => m.Owner).Returns(leader);
+            leaseProvider.Setup(m => m.GetLease()).Returns(lease.Object);
+
+            var messageHub = new Mock<IIntercomMessageHub>();
+            var logger = new Mock<ILogger>();
+            var listener = new Listener(s => { }, logger.Object);
+            messageHub.Setup(c => c.Subscribe()).Returns(listener);
+
+            var nodeResolver = new Mock<INodeResolver>();
+            var acceptorProcess = new Process(12);
+            nodeResolver.Setup(m => m.ResolveLocalNode()).Returns(acceptorProcess);
+
+            using (var acceptor = new Acceptor(messageHub.Object, null, leaseProvider.Object, nodeResolver.Object, logger.Object))
+            {
+                SendAcceptMessage(proposerId, proposedLogIndex, proposalNumber, listener);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof(RsmNackAcceptNotLeader) == msg.GetType())),
+                              Times.Exactly(1));
+        }
+
+        [Test]
+        public void OnAcceptRequestForChosenEntry_AcceptorRespondsWith_RsmNackAcceptChosen()
+        {
+            var leaderId = 1;
+            var proposedLogIndex = 120UL;
+            var proposalNumber = 100UL;
+
+            var leaseProvider = new Mock<ILeaseProvider>();
+            var lease = new Mock<ILease>();
+            var leader = new Process(leaderId);
+            lease.Setup(m => m.Owner).Returns(leader);
+            leaseProvider.Setup(m => m.GetLease()).Returns(lease.Object);
+
+            var messageHub = new Mock<IIntercomMessageHub>();
+            var logger = new Mock<ILogger>();
+            var listener = new Listener(s => { }, logger.Object);
+            messageHub.Setup(c => c.Subscribe()).Returns(listener);
+
+            var nodeResolver = new Mock<INodeResolver>();
+            var acceptorProcess = new Process(12);
+            nodeResolver.Setup(m => m.ResolveLocalNode()).Returns(acceptorProcess);
+
+            var replicatedLog = new Mock<IReplicatedLog>();
+            replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
+                         .Returns(new LogEntry(null, new LogIndex(proposedLogIndex), LogEntryState.Chosen));
+
+            using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
+            {
+                SendAcceptMessage(leaderId, proposedLogIndex, proposalNumber, listener);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof(RsmNackAcceptChosen) == msg.GetType())),
+                              Times.Exactly(1));
+        }
+
+        [Test]
+        public void OnAcceptRequestWithBallotThatLessThanMinAcceptorBallot_AcceptorRespondsWith_RsmNackAcceptBlocked()
+        {
+            var leaderId = 1;
+            var proposedLogIndex = 120UL;
+            var proposalNumber = 100UL;
+
+            var leaseProvider = new Mock<ILeaseProvider>();
+            var lease = new Mock<ILease>();
+            var leader = new Process(leaderId);
+            lease.Setup(m => m.Owner).Returns(leader);
+            leaseProvider.Setup(m => m.GetLease()).Returns(lease.Object);
+
+            var messageHub = new Mock<IIntercomMessageHub>();
+            var logger = new Mock<ILogger>();
+            var listener = new Listener(s => { }, logger.Object);
+            messageHub.Setup(c => c.Subscribe()).Returns(listener);
+
+            var nodeResolver = new Mock<INodeResolver>();
+            var acceptorProcess = new Process(12);
+            nodeResolver.Setup(m => m.ResolveLocalNode()).Returns(acceptorProcess);
+
+            var replicatedLog = new Mock<IReplicatedLog>();
+            replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
+                         .Returns(new LogEntry(null, new LogIndex(proposedLogIndex), LogEntryState.Accepted));
+
+            using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
+            {
+                SendAcceptMessage(leaderId, proposedLogIndex, proposalNumber, listener);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof(RsmAckAccept) == msg.GetType())),
+                                  Times.Exactly(1));
+
+                SendAcceptMessage(leaderId, proposedLogIndex, proposalNumber - 1, listener);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof(RsmNackAcceptBlocked) == msg.GetType())),
+                              Times.Exactly(1));
+        }
+
+        private static void SendAcceptMessage(int leaderId, ulong logIndex, ulong proposalNumber, Listener listener)
+        {
+            var rsmAccept = new RsmAccept(new wacs.Messaging.Messages.Process {Id = leaderId},
+                                          new RsmAccept.Payload
+                                          {
+                                              Leader = new wacs.Messaging.Messages.Process {Id = leaderId},
+                                              LogIndex = new wacs.Messaging.Messages.Intercom.Rsm.LogIndex { Index = logIndex },
+                                              Proposal = new Ballot{ProposalNumber = proposalNumber},
+                                              Value = new Message(null, null)
+                                          });
+            listener.Notify(rsmAccept);
+        }
+
+    }
+}
--- src/tests/Unit/Rsm/AcceptorPreparePhaseTests.cs
+++ src/tests/Unit/Rsm/AcceptorPreparePhaseTests.cs
@@ -12,52 +12,14 @@ using wacs.Resolver;
 using wacs.Rsm.Implementation;
 using wacs.Rsm.Interface;
 using Ballot = wacs.Messaging.Messages.Intercom.Rsm.Ballot;
-using LogIndex = wacs.Messaging.Messages.Intercom.Rsm.LogIndex;
+using LogIndex = wacs.Rsm.Implementation.LogIndex;
 using Process = wacs.Configuration.Process;
 
 namespace tests.Unit.Rsm
 {
     [TestFixture]
-    public class AcceptorTests
+    public class AcceptorPreparePhaseTests
     {
-        [Test]
-        public void OnAcceptRequestCameNotFromLeader_AcceptorRespondsWith_RsmNackAcceptNotLeader()
-        {
-            var leaderId = 1;
-            var proposerId = 2;
-
-            var leaseProvider = new Mock<ILeaseProvider>();
-            var lease = new Mock<ILease>();
-            var leader = new Process(leaderId);
-            lease.Setup(m => m.Owner).Returns(leader);
-            leaseProvider.Setup(m => m.GetLease()).Returns(lease.Object);
-
-            var messageHub = new Mock<IIntercomMessageHub>();
-            var logger = new Mock<ILogger>();
-            var listener = new Listener(s => { }, logger.Object);
-            messageHub.Setup(c => c.Subscribe()).Returns(listener);
-
-            var nodeResolver = new Mock<INodeResolver>();
-            var acceptorProcess = new Process(12);
-            nodeResolver.Setup(m => m.ResolveLocalNode()).Returns(acceptorProcess);
-
-            using (var acceptor = new Acceptor(messageHub.Object, null, leaseProvider.Object, nodeResolver.Object, logger.Object))
-            {
-                listener.Notify(new RsmAccept(new wacs.Messaging.Messages.Process { Id = proposerId },
-                                              new RsmAccept.Payload
-                                              {
-                                                  Leader = new wacs.Messaging.Messages.Process { Id = proposerId },
-                                                  LogIndex = new LogIndex(),
-                                                  Proposal = new Ballot(),
-                                                  Value = new Message(null, null)
-                                              }));
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
-
-            messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof(RsmNackAcceptNotLeader) == msg.GetType())),
-                              Times.Exactly(1));
-        }
-
         [Test]
         public void OnPrepareRequestCameNotFromLeader_AcceptorRespondsWith_RsmNackPrepareNotLeader()
         {
@@ -90,7 +52,6 @@ namespace tests.Unit.Rsm
             messageHub.Verify(m => m.Send(It.IsAny<IProcess>(), It.Is<IMessage>(msg => typeof (RsmNackPrepareNotLeader) == msg.GetType())),
                               Times.Exactly(1));
         }
-        
 
         [Test]
         public void OnPrepareRequestForChosenEntry_AcceptorRespondsWith_RsmNackPrepareChosen()
@@ -117,10 +78,10 @@ namespace tests.Unit.Rsm
 
             var replicatedLog = new Mock<IReplicatedLog>();
             replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
-                         .Returns(new LogEntry(null, new wacs.Rsm.Implementation.LogIndex(proposedLogIndex), LogEntryState.Chosen));
+                         .Returns(new LogEntry(null, new LogIndex(proposedLogIndex), LogEntryState.Chosen));
 
             using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
-            {             
+            {
                 SendPrepareMessage(leaderId, proposedLogIndex, proposalNumber, listener);
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
@@ -130,7 +91,7 @@ namespace tests.Unit.Rsm
         }
 
         [Test]
-        public void OnPrepareRequestWithHigherBallotForAcceptedEntry_AcceptorRespondsWith_RsmNackPrepareChosen()
+        public void OnPrepareRequestWithHigherBallotForAcceptedEntry_AcceptorRespondsWith_RsmAckPrepare()
         {
             var leaderId = 1;
             var proposedLogIndex = 120UL;
@@ -159,7 +120,7 @@ namespace tests.Unit.Rsm
             request.Setup(m => m.Body).Returns(new Body());
             syncCommand.Setup(m => m.Request).Returns(request.Object);
             replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
-                         .Returns(new LogEntry(syncCommand.Object, new wacs.Rsm.Implementation.LogIndex(proposedLogIndex), LogEntryState.Accepted));
+                         .Returns(new LogEntry(syncCommand.Object, new LogIndex(proposedLogIndex), LogEntryState.Accepted));
 
             using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
             {
@@ -201,7 +162,7 @@ namespace tests.Unit.Rsm
             request.Setup(m => m.Body).Returns(new Body());
             syncCommand.Setup(m => m.Request).Returns(request.Object);
             replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
-                         .Returns(new LogEntry(syncCommand.Object, new wacs.Rsm.Implementation.LogIndex(proposedLogIndex), LogEntryState.Accepted));
+                         .Returns(new LogEntry(syncCommand.Object, new LogIndex(proposedLogIndex), LogEntryState.Accepted));
 
             using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
             {
@@ -248,7 +209,7 @@ namespace tests.Unit.Rsm
             request.Setup(m => m.Body).Returns(new Body());
             syncCommand.Setup(m => m.Request).Returns(request.Object);
             replicatedLog.Setup(m => m.GetLogEntry(It.Is<ILogIndex>(l => l.Index == proposedLogIndex)))
-                         .Returns(new LogEntry(syncCommand.Object, new wacs.Rsm.Implementation.LogIndex(proposedLogIndex), LogEntryState.Accepted));
+                         .Returns(new LogEntry(syncCommand.Object, new LogIndex(proposedLogIndex), LogEntryState.Accepted));
 
             using (var acceptor = new Acceptor(messageHub.Object, replicatedLog.Object, leaseProvider.Object, nodeResolver.Object, logger.Object))
             {
@@ -265,13 +226,13 @@ namespace tests.Unit.Rsm
                               Times.Exactly(1));
         }
 
-        private static void SendPrepareMessage(int leaderId, ulong proposedLogIndex, ulong proposalNumber, Listener listener)
+        private static void SendPrepareMessage(int leaderId, ulong logIndex, ulong proposalNumber, Listener listener)
         {
             var rsmPrepare = new RsmPrepare(new wacs.Messaging.Messages.Process {Id = leaderId},
                                             new RsmPrepare.Payload
                                             {
                                                 Leader = new wacs.Messaging.Messages.Process {Id = leaderId},
-                                                LogIndex = new LogIndex {Index = proposedLogIndex},
+                                                LogIndex = new wacs.Messaging.Messages.Intercom.Rsm.LogIndex {Index = logIndex},
                                                 Proposal = new Ballot {ProposalNumber = proposalNumber}
                                             });
 
